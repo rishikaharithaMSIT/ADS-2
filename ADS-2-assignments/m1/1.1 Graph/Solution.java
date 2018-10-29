71082950f67bd29f991328064f1989f590648038
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-29 17:31:55 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -0,0 +1,8 @@
+interface Graph {
+    public int V();
+    public int E();
+    public void addEdge(int v, int w);
+    public Iterable<Integer> adj(int v);
+    public boolean hasEdge(int v, int w);
+}
+
