bf0b8139b7db3d211aaadfc09e3852144de52740
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-29 20:10:00 +0530

    "ads2_m1_a1_1: 4/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m1/1.1 Graph/Solution.java
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -23,12 +23,12 @@ class Matrix implements Graph {
 		return edges;
 	}
 	public void addEdge(int v, int w) {
-		if(v != w && !hasEdge(v,w)) {
+		if (v != w && !hasEdge(v, w)) {
 			matrix[v][w] = 1;
 			matrix[w][v] = 1;
 			edges++;
 		}
-		
+
 
 	}
 	public Iterable<Integer> adj(int v) {
@@ -58,30 +58,39 @@ class Matrix implements Graph {
 public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String Adjacency = scan.nextLine();
+		String adjacency = scan.nextLine();
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
-		
-		
-
-		Matrix matrix = new Matrix(vertices, edges);
-		String[] elements = scan.nextLine().split(",");
-		while (scan.hasNext()) {
-			String[] edge = scan.nextLine().split(" ");
-			matrix.addEdge(Integer.parseInt(edge[0]), Integer.parseInt(edge[1]));
-		}
-		System.out.println(matrix.vertices+ " vertices, "+ matrix.edges +" edges");
-		if(matrix.vertices <= 1 || matrix.edges <= 1) {
-			System.out.println("No edges");
-			return;
-		}
-		for (int i = 0; i < vertices; i++) {
-			for (int j = 0; j < vertices; j++) {
-				//int elem = matrix.matrix[i][j] ? 1 : 0;
-				System.out.print(matrix.matrix[i][j]  + " ");
+		switch (adjacency) {
+		case "List" :
+			break;
+		case "Matrix" :
+			Matrix matrix = new Matrix(vertices, edges);
+			String[] elements = scan.nextLine().split(",");
+			while (scan.hasNext()) {
+				String[] edge = scan.nextLine().split(" ");
+				matrix.addEdge(Integer.parseInt(edge[0]), Integer.parseInt(edge[1]));
 			}
-			System.out.println();
+			System.out.println(matrix.vertices + " vertices, " + matrix.edges + " edges");
+			if (matrix.vertices <= 1 || matrix.edges <= 1) {
+				System.out.println("No edges");
+				return;
+			}
+			for (int i = 0; i < vertices; i++) {
+				for (int j = 0; j < vertices; j++) {
+					//int elem = matrix.matrix[i][j] ? 1 : 0;
+					System.out.print(matrix.matrix[i][j]  + " ");
+				}
+				System.out.println();
+			}
+			break;
+		default:
 		}
+
+
+
+
+
 	}
 
 }
