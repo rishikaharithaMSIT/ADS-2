56b84b242745722e11fb74701f70ba685dfa9487
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-02 15:26:21 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -7,14 +7,14 @@ public class Solution {
 		String fileName2 = "Files\\" + StdIn.readLine();
 		In file1 = new In(fileName1);
 		In file2 = new In(fileName2);
-		int verticies = 0;
+		int vertices = 0;
 		Hashtable hashtable = new Hashtable();
 		while (file1.hasNextLine()) {
 			String[] lines = file1.readLine().split(",");
 			hashtable.put(Integer.parseInt(lines[0]), lines[1]);
-			verticies++;
+			vertices++;
 		}
-		Digraph diGraph = new Digraph(verticies);
+		Digraph diGraph = new Digraph(vertices);
 		while (file2.hasNextLine()) {
 			String[] lines = file2.readLine().split(",");
 			int v = Integer.parseInt(lines[0]);
@@ -23,8 +23,8 @@ public class Solution {
 			}
 		}
 		String command = StdIn.readLine();
-		System.out.println(diGraph.V()+" verticies, "+ diGraph.E()+ " edges");
-		for (int i = 0; i < verticies; i++) {
+		System.out.println(diGraph.V()+" vertices, "+ diGraph.E()+ " edges");
+		for (int i = 0; i < vertices; i++) {
 			System.out.print(i + ": ");
 			for (Integer k : diGraph.adj(i)) {
 				System.out.print(k + " ");
