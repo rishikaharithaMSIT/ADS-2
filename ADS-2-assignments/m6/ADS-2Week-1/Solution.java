be79725b87095395d9c0d53416c979c43aa5f938
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-03 11:16:46 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -1,14 +1,36 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
+
 class PageRank {
 	int outDegree;
 	int inDegree;
+	Digraph dg;
+	Hashtable<Integer, ArrayList<Integer>> incomingVertices;
 	PageRank(Digraph digraph, Integer vertex) {
 		this.outDegree = digraph.outdegree(vertex);
 		this.inDegree = digraph.indegree(vertex);
-		System.out.println(outDegree + " - " + inDegree);
+		this.dg = digraph;
+		incomingVertices = new Hashtable<Integer, ArrayList<Integer>>();
+		for (int i = 0; i < digraph.V(); i++) {
+			for (Integer k : digraph.adj(i)) {
+				if (incomingVertices.contains(k)) {
+					incomingVertices.get(k).add(i);
+				} else {
+					incomingVertices.put(k , new ArrayList<Integer>());
+					incomingVertices.get(k).add(i);
+				}
+			}
+		}
+		for (Integer l : incomingVertices.keySet()) {
+			System.out.println(incomingVertices.get(l));
+		}
+		//System.out.println(outDegree + " - " + inDegree);
 	}
 	// double getPR(int vertex) {
+	// 	if (outDegree == 0) {
+	// 		return 0.0;
+	// 	}
+	// 	double initial = 1 / dg.V();
 
 	// }
 
@@ -23,23 +45,23 @@ public class Solution {
 	public static void main(String[] args) {
 		// read the first line of the input to get the number of vertices
 
-		// iterate count of vertices times 
+		// iterate count of vertices times
 		// to read the adjacency list from std input
 		// and build the graph
-		
-		
+
+
 		// Create page rank object and pass the graph object to the constructor
-		
+
 		// print the page rank object
-		
+
 		// This part is only for the final test case
-		
+
 		// File path to the web content
 		String file = "WebContent.txt";
-		
+
 		// instantiate web search object
 		// and pass the page rank object and the file path to the constructor
-		
+
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
@@ -48,25 +70,25 @@ public class Solution {
 		int vertices = Integer.parseInt(scan.nextLine());
 		Digraph digraph = new Digraph(vertices);
 		int verticesCopy = vertices;
-		while(verticesCopy > 0 ) {
+		while (verticesCopy > 0 ) {
 			String[] vertex = scan.nextLine().split(" ");
 			int v = Integer.parseInt(vertex[0]);
-			for(int w = 1; w < vertex.length; w++) {
+			for (int w = 1; w < vertex.length; w++) {
 				digraph.addEdge(v, Integer.parseInt(vertex[w]));
 			}
 			verticesCopy--;
 		}
 		ArrayList<PageRank> prList = new ArrayList<>();
-		System.out.println(digraph.V() + " vertices, "+ digraph.E() + " edges ");
-		for(int i = 0; i < vertices; i++) {
+		System.out.println(digraph.V() + " vertices, " + digraph.E() + " edges ");
+		for (int i = 0; i < vertices; i++) {
 			System.out.print(i + ": ");
 			PageRank pr = new PageRank(digraph, i);
 			prList.add(pr);
-			for(Integer k : digraph.adj(i)) {
+			for (Integer k : digraph.adj(i)) {
 				System.out.print(k + " ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 }
