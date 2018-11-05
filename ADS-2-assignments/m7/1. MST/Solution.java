9305b1b4096d49b2e40bdd2ed6589fa25079f72b
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-05 22:56:03 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m7/1. MST/Solution.java
+++ ADS-2-assignments/m7/1. MST/Solution.java
@@ -1,38 +1,37 @@
 import java.util.Scanner;
-import java.util.Random;
 /**
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
 
-		while (scan.hasNext()) {
-			String[] line = scan.nextLine().split(" ");
-			Edge edge = new Edge(Integer.parseInt(line[0]),
-			                     Integer.parseInt(line[1]),
-			                     Double.parseDouble(line[2]));
-			ewg.addEdge(edge);
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(" ");
+            Edge edge = new Edge(Integer.parseInt(line[0]),
+                                 Integer.parseInt(line[1]),
+                                 Double.parseDouble(line[2]));
+            ewg.addEdge(edge);
 
-		}
-		KruskalMST mst = new KruskalMST(ewg);
-		System.out.format("%.5f", mst.weight());
-		//String.format("%.5f", weight);
+        }
+        KruskalMST mst = new KruskalMST(ewg);
+        System.out.format("%.5f", mst.weight());
+        //String.format("%.5f", weight);
 
-	}
+    }
 }
