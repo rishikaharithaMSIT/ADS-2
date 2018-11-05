56c952bc8472d591dab24098f6f542fe1e413732
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-05 21:57:58 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m7/1. MST/Solution.java
+++ ADS-2-assignments/m7/1. MST/Solution.java
@@ -5,13 +5,16 @@ public class Solution {
 		Scanner scan = new Scanner(System.in);
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
-
+		EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
 		while (scan.hasNext()) {
 			String[] line = scan.nextLine().split(" ");
 			Edge edge = new Edge(Integer.parseInt(line[0]),
 			                     Integer.parseInt(line[1]),
 			                     Double.parseDouble(line[2]));
+			ewg.addEdge(edge);
+
 		}
+		
 	}
 }
 class Edge implements Comparable<Edge> {
@@ -865,32 +868,32 @@ final class StdRandom {
         }
     }
 
-    /**
-     * Unit tests the methods in this class.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        if (args.length == 2) StdRandom.setSeed(Long.parseLong(args[1]));
-        double[] probabilities = { 0.5, 0.3, 0.1, 0.1 };
-        int[] frequencies = { 5, 3, 1, 1 };
-        String[] a = "A B C D E F G".split(" ");
+    // /**
+    //  * Unit tests the methods in this class.
+    //  *
+    //  * @param args the command-line arguments
+    //  */
+    // public static void main(String[] args) {
+    //     int n = Integer.parseInt(args[0]);
+    //     if (args.length == 2) StdRandom.setSeed(Long.parseLong(args[1]));
+    //     double[] probabilities = { 0.5, 0.3, 0.1, 0.1 };
+    //     int[] frequencies = { 5, 3, 1, 1 };
+    //     String[] a = "A B C D E F G".split(" ");
 
-        StdOut.println("seed = " + StdRandom.getSeed());
-        for (int i = 0; i < n; i++) {
-            StdOut.printf("%2d ",   uniform(100));
-            StdOut.printf("%8.5f ", uniform(10.0, 99.0));
-            StdOut.printf("%5b ",   bernoulli(0.5));
-            StdOut.printf("%7.5f ", gaussian(9.0, 0.2));
-            StdOut.printf("%1d ",   discrete(probabilities));
-            StdOut.printf("%1d ",   discrete(frequencies));
-            StdOut.printf("%11d ",  uniform(100000000000L));
-            StdRandom.shuffle(a);
-            for (String s : a)
-                StdOut.print(s);
-            StdOut.println();
-        }
-    }
+    //     StdOut.println("seed = " + StdRandom.getSeed());
+    //     for (int i = 0; i < n; i++) {
+    //         StdOut.printf("%2d ",   uniform(100));
+    //         StdOut.printf("%8.5f ", uniform(10.0, 99.0));
+    //         StdOut.printf("%5b ",   bernoulli(0.5));
+    //         StdOut.printf("%7.5f ", gaussian(9.0, 0.2));
+    //         StdOut.printf("%1d ",   discrete(probabilities));
+    //         StdOut.printf("%1d ",   discrete(frequencies));
+    //         StdOut.printf("%11d ",  uniform(100000000000L));
+    //         StdRandom.shuffle(a);
+    //         for (String s : a)
+    //             StdOut.print(s);
+    //         StdOut.println();
+    //     }
+    // }
 
 }
