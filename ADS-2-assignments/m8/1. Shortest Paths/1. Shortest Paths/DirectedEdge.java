1e7b5a3386c182e8fe24547d73c5527435b0a92f
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-06 22:55:18 +0530

    "ads2_m8_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m8/1. Shortest Paths/1. Shortest Paths/DirectedEdge.java
+++ ADS-2-assignments/m8/1. Shortest Paths/1. Shortest Paths/DirectedEdge.java
@@ -2,73 +2,73 @@
  * Class for directed edge.
  */
 public class DirectedEdge {
-	/**
-	 * v.
-	 */
-	private final int v;
-	/**
-	 * w.
-	 */
-	private final int w;
-	/**
-	 * weight.
-	 */
-	private final double weight;
+    /**
+     * v.
+     */
+    private final int v;
+    /**
+     * w.
+     */
+    private final int w;
+    /**
+     * weight.
+     */
+    private final double weight;
 
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      v       { parameter_description }
-	 * @param      w       { parameter_description }
-	 * @param      weight  The weight
-	 */
-	public DirectedEdge(int v, int w, double weight) {
-		if (v < 0) {
-			throw new IllegalArgumentException(
-			    "Vertex names must be nonnegative integers");
-		}
-		if (w < 0) {
-			throw new IllegalArgumentException(
-			    "Vertex names must be nonnegative integers");
-		}
-		if (Double.isNaN(weight)) {
-			throw new IllegalArgumentException(
-			    "Weight is NaN");
-		}
-		this.v = v;
-		this.w = w;
-		this.weight = weight;
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      v       { parameter_description }
+     * @param      w       { parameter_description }
+     * @param      weight  The weight
+     */
+    public DirectedEdge(final int v, final int w, final double weight) {
+        if (v < 0) {
+            throw new IllegalArgumentException(
+                "Vertex names must be nonnegative integers");
+        }
+        if (w < 0) {
+            throw new IllegalArgumentException(
+                "Vertex names must be nonnegative integers");
+        }
+        if (Double.isNaN(weight)) {
+            throw new IllegalArgumentException(
+                "Weight is NaN");
+        }
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
 
-	/**
-	 * Returns the tail vertex of the directed edge.
-	 * @return the tail vertex of the directed edge
-	 */
-	public int from() {
-		return v;
-	}
+    /**
+     * Returns the tail vertex of the directed edge.
+     * @return the tail vertex of the directed edge
+     */
+    public int from() {
+        return v;
+    }
 
-	/**
-	 * Returns the head vertex of the directed edge.
-	 * @return the head vertex of the directed edge
-	 */
-	public int to() {
-		return w;
-	}
+    /**
+     * Returns the head vertex of the directed edge.
+     * @return the head vertex of the directed edge
+     */
+    public int to() {
+        return w;
+    }
 
-	/**
-	 * Returns the weight of the directed edge.
-	 * @return the weight of the directed edge
-	 */
-	public double weight() {
-		return weight;
-	}
+    /**
+     * Returns the weight of the directed edge.
+     * @return the weight of the directed edge
+     */
+    public double weight() {
+        return weight;
+    }
 
-	/**
-	 * Returns a string representation of the directed edge.
-	 * @return a string representation of the directed edge
-	 */
-	public String toString() {
-		return v + "->" + w + " " + String.format("%5.2f", weight);
-	}
+    /**
+     * Returns a string representation of the directed edge.
+     * @return a string representation of the directed edge
+     */
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
 }
