b4e2a4c724697af0ed64e693c98a12f58fa8bd75
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-15 12:26:17 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m14/Assignment-1/Solution.java
+++ ADS-2-assignments/m14/Assignment-1/Solution.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-public class Solution {
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		Tst tst = new Tst();
-		for(int i =0; i < words.length;i++) {
-			for(int j =0; j < words[i].length(); j++) {
-				tst.put(words[i].substring(j), i);
-			}
-			
-		}
-		Scanner scan = new Scanner(System.in);
-		String prefixWord = scan.nextLine();
-		Iterable<String> prefixs = tst.keysWithPrefix(prefixWord);
-		for(String each:  prefixs) {
-			System.out.println(each);
-		}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here...
+        Tst tst = new Tst();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                tst.put(words[i].substring(j), i);
+            }
 
-	}
+        }
+        Scanner scan = new Scanner(System.in);
+        String prefixWord = scan.nextLine();
+        Iterable<String> prefixs
+            = tst.keysWithPrefix(prefixWord);
+        for (String each :  prefixs) {
+            System.out.println(each);
+        }
 
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    }
+    /**
+     * Loads words.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
