10acf366a0550e35c00e50cb807dc358e7469664
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-16 10:18:25 +0530

    "ads2_m16_a1_1: 0/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m16/boggle/BoggleSolver.java
+++ ADS-2-assignments/m16/boggle/BoggleSolver.java
@@ -14,19 +14,28 @@ public class BoggleSolver {
 	}
 
 	// Returns the set of all valid words in the given Boggle board, as an Iterable.
+	// public Iterable<String> getAllValidWords(BoggleBoard board) {
+
+	// 	Bag<String> bag = new Bag<String>();
+	// 	for (String each : tset) {
+	// 		boolean possible = isValid(each, board);
+	// 		if (possible) {
+	// 			bag.add(each);
+	// 		}
+	// 	}
+	// 	// System.out.println(bag.size());
+	// 	// for (String word : bag) {
+	// 	// 	System.out.println(word);
+	// 	// }
+	// 	return bag;
+	// }
 	public Iterable<String> getAllValidWords(BoggleBoard board) {
 
 		Bag<String> bag = new Bag<String>();
-		for (String each : tset) {
-			boolean possible = isValid(each, board);
-			if (possible) {
-				bag.add(each);
-			}
-		}
-		// System.out.println(bag.size());
-		// for (String word : bag) {
-		// 	System.out.println(word);
-		// }
+		marked = new boolean[board.rows()][board.cols()];
+		String str = "";
+		getWords(0, 0, board, str);
+		System.out.println(str);
 		return bag;
 	}
 
@@ -34,107 +43,126 @@ public class BoggleSolver {
 	// (You can assume the word contains only the uppercase letters A through Z.)
 	public int scoreOf(String word) {
 		int wordlen = word.length();
-		switch(wordlen) {
-			case 0:
-			case 1:
-			case 2: return 0;
-			case 3:
-			case 4: return 1;
-			case 5: return 2;
-			case 6: return 3;
-			case 7: return 5;
-			default: return 11;
+		switch (wordlen) {
+		case 0:
+		case 1:
+		case 2: return 0;
+		case 3:
+		case 4: return 1;
+		case 5: return 2;
+		case 6: return 3;
+		case 7: return 5;
+		default: return 11;
 		}
-		
+
 	}
-	boolean isValid(String word, BoggleBoard board) {
-		//String[] letters = word.split("");
-		for (int i = 0; i < board.rows(); i++) {
-			for (int  j = 0; j < board.cols(); j++) {
-				
-				if (word.charAt(0) == board.getLetter(i, j)) {
-					marked = new boolean[board.rows()][board.cols()];
-					index = 0;
-					boolean yes = isValid(i, j, marked, board, word);
-					if (yes) return true;
+
+	void getWords(int r, int c, BoggleBoard board, String str) {
+		marked[r][c] = true;
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				int v = r + i;
+				int w = c + j;
+				if (v >= 0 && w >= 0 && v < board.rows() && w < board.cols()) {
+					if(!marked[v][w]){
+						str = str + board.getLetter(v,w);
+						getWords(r,c,board,str);
+					}
 				}
 			}
 		}
-		return false;
+		marked[r][c] = false;
 	}
-	boolean isValid(int i, int j, boolean[][] marked, BoggleBoard board, String word) {
-		index++;
-		//System.out.println(index);
-		if (index == word.length()) return true;
-		if (i >= 0 && j >= 0 && i < board.rows() && j < board.cols()) {
-			marked[i][j] = true;
-		}
-		int a = i;
-		int b = j;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				return true;
 
-			}
-		}
-		a = i - 1;
-		b = j - 1;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i - 1;
-		b = j;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i + 1;
-		b = j + 1;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i;
-		b = j - 1;
-		if (a > 0 && b > 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i;
-		b = j + 1;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i + 1;
-		b = j - 1;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i + 1;
-		b = j;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		a = i + 1;
-		b = j + 1;
-		if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
-			if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
-				if(isValid(a, b, marked, board, word)) return true;
-			}
-		}
-		return false;
-	}
+	// boolean isValid(String word, BoggleBoard board) {
+	// 	//String[] letters = word.split("");
+	// 	for (int i = 0; i < board.rows(); i++) {
+	// 		for (int  j = 0; j < board.cols(); j++) {
+
+	// 			if (word.charAt(0) == board.getLetter(i, j)) {
+	// 				marked = new boolean[board.rows()][board.cols()];
+	// 				index = 0;
+	// 				boolean yes = isValid(i, j, marked, board, word);
+	// 				if (yes) return true;
+	// 			}
+	// 		}
+	// 	}
+	// 	return false;
+	// }
+	// boolean isValid(int i, int j, boolean[][] marked, BoggleBoard board, String word) {
+	// 	index++;
+	// 	//System.out.println(index);
+	// 	if (index == word.length()) return true;
+	// 	if (i >= 0 && j >= 0 && i < board.rows() && j < board.cols()) {
+	// 		marked[i][j] = true;
+	// 	}
+	// 	// int a = i;
+	// 	// int b = j;
+	// 	// if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 	// 	if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 	// 		return true;
+
+	// 	// 	}
+	// 	// }
+	// 	//
+	// 	a = i - 1;
+	// 	b = j - 1;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i - 1;
+	// 	b = j;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i + 1;
+	// 	b = j + 1;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i;
+	// 	b = j - 1;
+	// 	if (a > 0 && b > 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i;
+	// 	b = j + 1;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i + 1;
+	// 	b = j - 1;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i + 1;
+	// 	b = j;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	a = i + 1;
+	// 	b = j + 1;
+	// 	if (a >= 0 && b >= 0 && a < board.rows() && b < board.cols()) {
+	// 		if (board.getLetter(a, b) == word.charAt(index) && !marked[a][b]) {
+	// 			if(isValid(a, b, marked, board, word)) return true;
+	// 		}
+	// 	}
+	// 	return false;
+	// }
 
 
 }
