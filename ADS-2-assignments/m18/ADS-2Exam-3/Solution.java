4fb1ea1b28e6892938849aa35f5f2f70843a5f30
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-11-17 13:03:32 +0530

    "ads2_m18_a1_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m18/ADS-2Exam-3/Solution.java
+++ ADS-2-assignments/m18/ADS-2Exam-3/Solution.java
@@ -5,17 +5,27 @@ import java.util.Arrays;
 /**
  * Class for solution.
  */
-public class Solution {
-
-	// Don't modify this method.
-	public static void main(String[] args) {
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused.
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String cases = scan.nextLine();
 
 		switch (cases) {
 		case "loadDictionary":
 			// input000.txt and output000.txt
-			BinarySearchST<String, Integer> hash = loadDictionary("/Files/t9.csv");
+			BinarySearchST<String, Integer> hash
+			    = loadDictionary("/Files/t9.csv");
 			while (scan.hasNextLine()) {
 				String key = scan.nextLine();
 				System.out.println(hash.get(key));
@@ -89,8 +99,10 @@ public class Solution {
 		return in.readAllStrings();
 	}
 
-	public static BinarySearchST<String, Integer> loadDictionary(String file) {
-		BinarySearchST<String, Integer>  st = new BinarySearchST<String, Integer>();
+	public static BinarySearchST<String, Integer> loadDictionary(
+	    final String file) {
+		BinarySearchST<String, Integer>  st
+		    = new BinarySearchST<String, Integer>();
 		// your code goes here
 		String[] dictionary = toReadFile(file);
 		for (int i = 0; i < dictionary.length; i++) {
@@ -126,42 +138,42 @@ class T9 {
 		// your code goes here
 		// String[] nums = t9Signature.split("");
 		ArrayList<String> list = new ArrayList<>();
-		for(String each: tst.keys()) {
+		for (String each : tst.keys()) {
 			String[] word = each.split("");
 			String num = "";
-			for(String ch : word){
-				if(ch.equals("a") || ch.equals("b") || ch.equals("c")) {
+			for (String ch : word) {
+				if (ch.equals("a") || ch.equals("b") || ch.equals("c")) {
 					num = num + "2";
 				}
-				if(ch.equals("d") || ch.equals("e") || ch.equals("f")) {
+				if (ch.equals("d") || ch.equals("e") || ch.equals("f")) {
 					num = num + "3";
 				}
-				if(ch.equals("g") || ch.equals("h") || ch.equals("i")) {
+				if (ch.equals("g") || ch.equals("h") || ch.equals("i")) {
 					num = num + "4";
 				}
-				if(ch.equals("j") || ch.equals("k") || ch.equals("l")) {
+				if (ch.equals("j") || ch.equals("k") || ch.equals("l")) {
 					num = num + "5";
 				}
-				if(ch.equals("m") || ch.equals("n") || ch.equals("o")) {
+				if (ch.equals("m") || ch.equals("n") || ch.equals("o")) {
 					num = num + "6";
 				}
-				if(ch.equals("p") || ch.equals("q") || ch.equals("r") || ch.equals("s")) {
+				if (ch.equals("p") || ch.equals("q") || ch.equals("r") || ch.equals("s")) {
 					num = num + "7";
 				}
-				if(ch.equals("t") || ch.equals("u") || ch.equals("v")) {
+				if (ch.equals("t") || ch.equals("u") || ch.equals("v")) {
 					num = num + "8";
 				}
-				if(ch.equals("w") || ch.equals("x") || ch.equals("y") || ch.equals("z")) {
+				if (ch.equals("w") || ch.equals("x") || ch.equals("y") || ch.equals("z")) {
 					num = num + "9";
 				}
 			}
-			if(num.equals(t9Signature)) {
-				if(tst.contains(each)) {
+			if (num.equals(t9Signature)) {
+				if (tst.contains(each)) {
 					list.add(each);
 				}
-				
+
 			}
-		} 
+		}
 		return list;
 	}
 
@@ -171,21 +183,21 @@ class T9 {
 		// System.out.print("Possible : ");
 		// for(String one : words) {
 		// 	System.out.print(one + " " + tst.get(one) + " ");
-		// }		
+		// }
 		//System.out.println();
-		
+
 		ArrayList<String> al = new ArrayList<>();
 		BinarySearchST<Integer, ArrayList<String>> bst = new BinarySearchST<>();
 		MaxPQ<Integer> maxpq = new MaxPQ<>();
-		for(String each : words) {
-			if(tst.contains(each)) {
+		for (String each : words) {
+			if (tst.contains(each)) {
 				maxpq.insert(tst.get(each));
 			}
-			
+
 		}
-		for(String each : words) {
+		for (String each : words) {
 			int freq = tst.get(each);
-			if(bst.contains(freq)) {
+			if (bst.contains(freq)) {
 				ArrayList<String> arr = bst.get(freq);
 				arr.add(each);
 				Collections.sort(arr);
@@ -198,26 +210,26 @@ class T9 {
 				bst.put(freq, arr);
 			}
 		}
-		int i =0;
+		int i = 0;
 
-		while(true) {
-			if(maxpq.isEmpty()) break;
+		while (true) {
+			if (maxpq.isEmpty()) break;
 			int m = maxpq.delMax();
 			ArrayList<String> arr2 = bst.get(m);
 			//System.out.println(arr2);
-			for(int j =0;j<arr2.size();j++) {
+			for (int j = 0; j < arr2.size(); j++) {
 				al.add(arr2.get(j));
 				i++;
 				//System.out.println(i + "-"+ k);
-				if(i >= k) break;
+				if (i >= k) break;
 			}
-			if(i >= k) break;	
-				
-			}	
+			if (i >= k) break;
 
-			
-			
-		
+		}
+
+
+
+
 
 		Collections.sort(al);
 		//System.out.println(al.size() + " - " + k);
